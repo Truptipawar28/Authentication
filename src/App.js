db472e35ad1ad1087d4bd95966c239e98564b766
Trupti~ <truppawar2801@gmail.com>
2023-07-09 14:32:05 +0530

    Signup and Authentication

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import { Switch } from 'react-router-dom/cjs/react-router-dom';
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
